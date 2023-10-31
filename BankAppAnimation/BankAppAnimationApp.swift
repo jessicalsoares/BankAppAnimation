4a20adb64cfe3c2fed913a657e2129d847d41d2c
Jessica Soares <jess.soares@hotmail.com>
2023-10-31 10:55:14 +0000

    Initial Commit

---
+++ BankAppAnimation/BankAppAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankAppAnimationApp.swift
+//  BankAppAnimation
+//
+//  Created by Jessica Soares on 31/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BankAppAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
